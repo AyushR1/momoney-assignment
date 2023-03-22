b7a2c58430f0e47ec8a37f61d70cdc4c1beb7cf7
AyushR1 <ayushrathore12501@gmail.com>
2023-03-22 22:45:18 +0530

    Create a server
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- server.go
+++ server.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := http.NewServeMux()
+	// Start the server
+	addr := ":8080"
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Server is running at port %s", addr)
+	})
+	log.Printf("Server running on http://localhost%s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, router))
 }
